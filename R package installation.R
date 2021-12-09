47bc63fb1703b3a8a3db61f31e6e0e1d24fbc26c
E-Y-M <EricYMah@users.noreply.github.com>
2021-12-09 13:26:36 -0800

    Created script to automate package installation for running local copies

---
+++ R package installation.R
@@ -0,0 +1,15 @@
+# Run this script to install all the required packages to run a local copy of powe(R)OC
+install.packages("pacman")
+
+library(pacman)
+
+pacman::p_load(
+    shiny,
+    shinyjs,
+    shinydashboard,
+    tidyverse,
+    readr,
+    pROC,
+    shinyBS,
+    googlesheets4
+)
